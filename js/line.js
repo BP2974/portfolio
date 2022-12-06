2f4e401a99bff1ee9fb012813b8879635d910839
Baek MinKyung <en0008@naver.com>
2022-12-06 23:34:41 +0900

    0.1.4

--- js/line.js
+++ js/line.js
@@ -1,24 +1,28 @@
 $(document).ready(function(){
     
     const content1 = document.querySelector('.content1')
+    const content2 = document.querySelector('.content2')
     const path1 = document.querySelector('.path1')
+    const path2 = document.querySelector('.path2')
     const path1Length = path1.getTotalLength()
-    console.log(path1Length)
-    console.log(content1.offsetHeight)
+    const path2Length = path2.getTotalLength()
 
     path1.style.strokeDasharray  = path1Length
-    path1.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.5, content1, path1Length)
+    path1.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.8, content1, path1Length)
+
+    path2.style.strokeDasharray  = path2Length
+    path2.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.2, content2, path2Length)
 
     function calcDashoffset(scrollY, element, length) {
         const ratio = (scrollY - element.offsetTop) / element.offsetHeight
         const value = length - (length * ratio)
-        console.log(value)
         return value < 0 ? 0 : value > length ? length : value
     }
 
     function scrollHandler() {
-        const scrollY = window.scrollY + (window.innerHeight * 0.5)
+        const scrollY = window.scrollY + (window.innerHeight * 0.8)
         path1.style.strokeDashoffset = calcDashoffset(scrollY, content1, path1Length)
+        path2.style.strokeDashoffset = calcDashoffset(scrollY, content2, path2Length)
     }
 
     window.addEventListener('scroll', scrollHandler)
