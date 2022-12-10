d1411850103bf692e5b738f8cc322f38e9e2e993
Baek MinKyung <en0008@naver.com>
2022-12-11 01:28:02 +0900

    0.1.5

--- js/line.js
+++ js/line.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
     
     const content1 = document.querySelector('.content1')
-    const content2 = document.querySelector('.content2')
+    /* const content2 = document.querySelector('.content2') */
     const path1 = document.querySelector('.path1')
-    const path2 = document.querySelector('.path2')
+    /* const path2 = document.querySelector('.path2') */
     const path1Length = path1.getTotalLength()
-    const path2Length = path2.getTotalLength()
+    /* const path2Length = path2.getTotalLength() */
+
 
     path1.style.strokeDasharray  = path1Length
-    path1.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.8, content1, path1Length)
+    path1.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.6, content1, path1Length)
 
-    path2.style.strokeDasharray  = path2Length
-    path2.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.2, content2, path2Length)
+    /* path2.style.strokeDasharray  = path2Length
+    path2.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.2, content2, path2Length) */
 
     function calcDashoffset(scrollY, element, length) {
         const ratio = (scrollY - element.offsetTop) / element.offsetHeight
@@ -20,12 +21,41 @@ $(document).ready(function(){
     }
 
     function scrollHandler() {
-        const scrollY = window.scrollY + (window.innerHeight * 0.8)
+        const scrollY = window.scrollY + (window.innerHeight * 0.6)
         path1.style.strokeDashoffset = calcDashoffset(scrollY, content1, path1Length)
-        path2.style.strokeDashoffset = calcDashoffset(scrollY, content2, path2Length)
+        /* path2.style.strokeDashoffset = calcDashoffset(scrollY, content2, path2Length) */
     }
 
     window.addEventListener('scroll', scrollHandler)
+    window.addEventListener('scroll', function(){
+
+        var ratio1 = 1/1920 * window.innerWidth;
+        let win_scroll = $(window).scrollTop()
+        console.log($(window).scrollTop())
+
+        if(win_scroll >= 600 * ratio1)
+        {
+            for(let i = 1; i < 4; i++){
+                $('.profile_path'+i).css('stroke-dashoffset', '0')
+            }
+        }
+        if(win_scroll >= 700 * ratio1)
+        {
+            for(let i = 4; i < 6; i++){
+                $('.profile_path'+i).css('stroke-dashoffset', '0')
+            }
+        }
+    })
+
+    $('.skill_tab li').click(function(){
+        const index = $(this).index();
+
+        $('.skill_tab li').removeClass('active');
+        $(this).addClass('active');
+        
+        $('.skill_con li').removeClass('active');
+        $('.skill_con li').eq(index).addClass('active');
+    });
 })
     
 
